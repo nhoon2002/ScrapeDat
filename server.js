356700aa924c01de051a1108074169175ca8d77c
NH\nhoon_000 <nhoon2002@ucla.edu>
2017-02-19 22:12:45 -0800

    Fixed port issue

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.Promise = Promise;
 
 // Initialize Express
 var app = express();
-
+var port = process.env.PORT || 3000;
 // Use morgan and body parser with our app
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
@@ -171,6 +171,6 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("App running on port 3000!");
 });
